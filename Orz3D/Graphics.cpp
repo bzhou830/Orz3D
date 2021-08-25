42bcf6e616325071093f58135cbfd01d9aff1d4b
bineanzhou <bineanzhou@glenfly.com>
2021-08-25 18:32:00 +0800

    顶点颜色使用8 bit per channel unorm

--- Orz3D/Graphics.cpp
+++ Orz3D/Graphics.cpp
@@ -88,18 +88,18 @@ void Graphics::DrawTestTriangle()
 		} position;
 		struct 
 		{
-			float r;
-			float g;
-			float b;
+			UCHAR r;
+			UCHAR g;
+			UCHAR b;
 		} color;
 	};
 
 	// create vertex buffer (1 2d triangle at center of screen)
 	const Vertex vertices[] =
 	{  
-		{ 0.0f,0.5f,1.0f,0.0f,0.0f },
-		{ 0.5f,-0.5f, 0.0f,1.0f,0.0f },
-		{ -0.5f,-0.5f, 0.0f,0.0f,1.0f },
+		{ 0.0f,0.5f,255,0,0 },
+		{ 0.5f,-0.5f, 0,255,0 },
+		{ -0.5f,-0.5f, 0,0,255 },
 	};
 	
 	D3D11_BUFFER_DESC bd = {};
@@ -135,7 +135,7 @@ void Graphics::DrawTestTriangle()
 	{
 		// "Position" 的声明和vs中的输入名称是一一对应的
 		{ "Position", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-		{ "Color", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, sizeof(Vertex::position), D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "Color", 0, DXGI_FORMAT_R8G8B8A8_UNORM, 0, sizeof(Vertex::position), D3D11_INPUT_PER_VERTEX_DATA, 0 },
 	};
 	GFX_THROW_INFO(pDevice->CreateInputLayout(
 		ied, (UINT)std::size(ied),
