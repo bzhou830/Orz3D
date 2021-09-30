ffa2a05a93ef9d395058e88b2a1aa9b89a1a2f27
bineanzhou <bineanzhou@glenfly.com>
2021-09-30 22:30:06 +0800

    选择使用图片或者使用视频作为Texture.

--- Orz3D/Surface.h
+++ Orz3D/Surface.h
@@ -4,6 +4,7 @@
 #include "opencv2/core/ocl.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/videoio.hpp"
+#include "opencv2/imgcodecs.hpp"
 
 using namespace cv;
 
@@ -12,15 +13,26 @@ class Surface
 public:
     Surface(std::string str)
     {
-        if (m_cap.isOpened() == false)
-            m_cap.open(str);
+        std::string::size_type pos = str.rfind('.');
+        std::string ext = str.substr(pos == std::string::npos ? str.length() : pos + 1);
+        if (ext == "mp4")
+        {
+            if (m_cap.isOpened() == false)
+                m_cap.open(str);
 
-        //设置视频的获取的位置
-        m_cap.set(CAP_PROP_POS_MSEC, static_cast<double>(rand() % 20) * 1000.f);
-        if (!m_cap.read(m_frame))
-            assert(0);
-        cv::cvtColor(m_frame, m_frame, cv::COLOR_RGB2RGBA);
-        cv::resize(m_frame, m_frame, cv::Size(800, 600));
+            //设置视频的获取的位置
+            m_cap.set(CAP_PROP_POS_MSEC, static_cast<double>(rand() % 20) * 1000.f);
+            if (!m_cap.read(m_frame))
+                assert(0);
+            cv::cvtColor(m_frame, m_frame, cv::COLOR_RGB2RGBA);
+            cv::resize(m_frame, m_frame, cv::Size(800, 600));
+        }
+        else if (ext == "png")
+        {
+            m_frame = cv::imread(str);
+            cv::cvtColor(m_frame, m_frame, cv::COLOR_RGB2RGBA);
+            cv::resize(m_frame, m_frame, cv::Size(800, 600));
+        }
     }
     ~Surface()
     {
